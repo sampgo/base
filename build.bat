87f850c52d2d536e861b600533ecf184960e607c
AliLogic <drfiniz@gmail.com>
2021-04-18 17:50:59 +0500

    Update: PS and Batch Scripts

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 set CGO_CFLAGS_ALLOW=".*"
+set CGO_LDFLAGS_ALLOW=".*"
 set CGO_ENABLED=1
 set GOOS=windows
 set GOARCH=386
 
-go get github.com/sampgo/sampgo@5e54b15
+go get github.com/sampgo/sampgo
 go build -buildmode=c-shared -o build/base.dll main.go 
--- build.ps1
+++ build.ps1
@@ -1,7 +1,8 @@
-$env:CGO_CFLAGS_ALLOW=".*"; 
+$env:CGO_CFLAGS_ALLOW=".*";
+$env:CGO_LDFLAGS_ALLOW=".*";
 $env:CGO_ENABLED=1; 
 $env:GOOS="windows"; 
 $env:GOARCH=386;
 
-go get -x github.com/sampgo/sampgo@5e54b15
+go get -x github.com/sampgo/sampgo
 go build -x -buildmode=c-shared -o build/base.dll main.go
