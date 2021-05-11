5afac9f0e330c0766ff292b45f556d6bfe92f9c6
AliLogic <drfiniz@gmail.com>
2021-05-11 15:50:28 +0500

    Add: GO111MODULE env for go get

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ set CGO_LDFLAGS_ALLOW=".*"
 set CGO_ENABLED=1
 set GOOS=windows
 set GOARCH=386
+set GO111MODULE="on"
 
-go get github.com/sampgo/sampgo
-go build -buildmode=c-shared -o build/base.dll main.go 
+go get -x github.com/sampgo/sampgo
+go build -x -buildmode=c-shared -o build/base.dll main.go
--- build.ps1
+++ build.ps1
@@ -3,6 +3,7 @@ $env:CGO_LDFLAGS_ALLOW=".*";
 $env:CGO_ENABLED=1; 
 $env:GOOS="windows"; 
 $env:GOARCH=386;
+$env:GO111MODULE="on";
 
 go get -x github.com/sampgo/sampgo
 go build -x -buildmode=c-shared -o build/base.dll main.go
